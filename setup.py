dbc79b731629166749c477510cbde75945f9c8bc
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-06-02 15:19:07 +0200

    Read version from a separate file

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
+import os
 import setuptools
+
+
+def _this_path():
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def _read_readme():
+    with open(os.path.join(_this_path(), "README.md")) as f:
+        return f.read()
+
+
+def _read_version():
+    with open(os.path.join(_this_path(), "VERSION")) as f:
+        return f.read().strip()
 
 
 setuptools.setup(
     name="versioned-package-sample",
-    version="0.1.0",
+    version=_read_version(),
     # author="Alexander Juda",
     # author_email="alexanderjuda@gmail.com",
     packages=setuptools.find_packages(
